332c85d270b891b780fae50d076a402e00204e3f
Tomáš Moravec <moravec-tomas@seznam.cz>
2017-10-31 16:07:15 +0100

    Added controls and UI

---
+++ Software/Master/Master/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Management;
+
+
+namespace Master
+{
+    public partial class Form1 : Form
+    {
+        #region Initialization
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeControls();
+        }
+
+        private void InitializeControls()
+        {
+            InitPorts();
+            InitBaudRates();
+        }
+
+        private int numberOfPorts = 0;
+
+        /// <summary>
+        /// Init ports for connection
+        /// </summary>
+        private void InitPorts()
+        {
+            // Get ports
+            string[] ports = SerialPort.GetPortNames();
+
+            // Save new ports if their are not same
+            if (ports.Length != numberOfPorts)
+            {
+                comboBoxPorts.Items.Clear();
+                foreach (string port in ports) comboBoxPorts.Items.Add(port);
+                if (ports.Length > 0) comboBoxPorts.SelectedIndex = 0;
+                this.numberOfPorts = ports.Length;
+            }
+        }
+
+        /// <summary>
+        /// Init baudRates for connection
+        /// </summary>
+        private void InitBaudRates()
+        {
+            // All baudrates
+            string[] baudRates = new string[] {
+                "4800",
+                "9600",
+                "19200",
+                "38400",
+                "57600",
+                "115200",
+            };
+
+            // Add all in comboBox
+            foreach (string baudRate in baudRates) comboBoxBaudRates.Items.Add(baudRate);
+
+            // Select index of wanted baudRate
+            for (int i = 0; i < baudRates.Length; i++)
+            {
+                if (comboBoxBaudRates.Items[i].ToString() == "9600")
+                {
+                    comboBoxBaudRates.SelectedIndex = i;
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Autocalled when selected port changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBoxPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SetDeviceName(comboBoxPorts.SelectedItem.ToString());
+        }
+
+        /// <summary>
+        /// Get device name on selected port
+        /// </summary>
+        /// <param name="portName">Port name</param>
+        private void SetDeviceName(string portName)
+        {
+            try
+            {
+                using (var searcher = new ManagementObjectSearcher("SELECT * FROM WIN32_SerialPort"))
+                {
+                    string[] portnames = SerialPort.GetPortNames();
+                    List<ManagementBaseObject> ports = searcher.Get().Cast<ManagementBaseObject>().ToList();
+
+                    foreach (ManagementBaseObject port in ports)
+                    {
+                        if (port["DeviceID"].ToString().Equals(portName))
+                        {
+                            textBoxDeviceName.Text = port["Description"].ToString();  // Hint: Win32_SerialPort class
+                            break;
+                        }
+                    }
+                }
+
+            }
+            catch
+            {
+                ;
+            }
+        }
+
+        #endregion Initialization
+
+        #region Bulb control
+
+        private Bitmap bulbOff = Properties.Resources.bulbOff;
+        private Bitmap bulbOn = Properties.Resources.bulbOn;
+
+        public void ChangeBulbStateImage(bool state)
+        {
+            pictureBox.Image = (state) ? bulbOn : bulbOff;
+        }
+
+        #endregion Bulb control
+
+
+        private void TrackBar_ValueChanged(object sender, EventArgs e)
+        {
+            labelTrackBarValue.Text = trackBar.Value.ToString();
+        }
+
+        
+    }
+}
---
+++ Task/Others/projektModbus/projektModbus/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace projektModbus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboPort = new System.Windows.Forms.ComboBox();
+            this.comboSpeed = new System.Windows.Forms.ComboBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.trackPot = new System.Windows.Forms.TrackBar();
+            this.led = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackPot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboPort
+            // 
+            this.comboPort.FormattingEnabled = true;
+            this.comboPort.Location = new System.Drawing.Point(12, 12);
+            this.comboPort.Name = "comboPort";
+            this.comboPort.Size = new System.Drawing.Size(121, 21);
+            this.comboPort.TabIndex = 0;
+            // 
+            // comboSpeed
+            // 
+            this.comboSpeed.FormattingEnabled = true;
+            this.comboSpeed.Location = new System.Drawing.Point(12, 52);
+            this.comboSpeed.Name = "comboSpeed";
+            this.comboSpeed.Size = new System.Drawing.Size(121, 21);
+            this.comboSpeed.TabIndex = 1;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(175, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 2;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(175, 50);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 3;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonClose_MouseClick);
+            // 
+            // trackPot
+            // 
+            this.trackPot.Location = new System.Drawing.Point(12, 105);
+            this.trackPot.Maximum = 1023;
+            this.trackPot.Name = "trackPot";
+            this.trackPot.Size = new System.Drawing.Size(182, 45);
+            this.trackPot.TabIndex = 4;
+            this.trackPot.TickFrequency = 75;
+            // 
+            // led
+            // 
+            this.led.BackColor = System.Drawing.Color.White;
+            this.led.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.led.Location = new System.Drawing.Point(242, 105);
+            this.led.Name = "led";
+            this.led.Size = new System.Drawing.Size(25, 24);
+            this.led.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 19200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ADR:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "ERR:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(136, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "REG:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(190, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "KOD:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 197);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.led);
+            this.Controls.Add(this.trackPot);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.comboSpeed);
+            this.Controls.Add(this.comboPort);
+            this.Name = "Form1";
+            this.Text = "MR2S";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackPot)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboPort;
+        private System.Windows.Forms.ComboBox comboSpeed;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.TrackBar trackPot;
+        private System.Windows.Forms.Panel led;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
